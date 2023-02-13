a5c449a00ca9f305ed1a79688764b805cc5c6ce4
Adam <adam.ewing.nz@gmail.com>
2023-02-13 08:16:55 +0800

    Added commented numbers for each question.

--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Variables {
+public class Variables {
+
+    public static void main(String[] args) {
+
+        // 1.
+
+        // 2.
+
+        // 3.
+
+        // 4.
+
+        // 5.
+
+        // 6.
+
+        // 7.
+
+        // 8.
+
+        // 9.
+
+        // 10.
+
+        // 11.
+
+        // 12.
+        /**
+         * [type answer here]
+         */
+
+    }
 }
