19e8ea367b0c23a92052db2a58756a342da82a2d
techarenz <57818506+techarenz@users.noreply.github.com>
2023-02-15 10:25:53 +0800

    Update README.md

--- src/Variables.java
+++ src/Variables.java
@@ -3,30 +3,44 @@ public class Variables {
     public static void main(String[] args) {
 
         // 1.
+        int age = 33;
 
         // 2.
+        System.out.println(age);
 
         // 3.
+        double balance = 18.89;
 
         // 4.
+        int dollars =  (int) balance;
 
         // 5.
+        int dollarsRounded = (int) balance + 1;
 
         // 6.
+        char letter = 'A';
 
         // 7.
+        System.out.println(letter);
 
         // 8.
+        System.out.println((int) letter);
 
         // 9.
+        letter += 1;
 
         // 10.
+        System.out.println(letter);
 
         // 11.
+        System.out.println((int) letter);
 
         // 12.
         /**
-         * [type answer here]
+         * [In questions 6 - 11, we are taking a character data type and narrow casting it to an integer.
+         * The resulted integer is equivalent to the ASCII value of the character. When we incremented the
+         * character type, Java is actually incrementing the ASCII value, and then returning the corresponding
+         * character to this new value. So, character 'A' has the ASCII value of 65, and ASCII value 66 refers to character 'B'.]
          */
 
     }
